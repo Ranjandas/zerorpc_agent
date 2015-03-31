231dc43f36f336c24193fa29e8dfee8fe7d0bf6b
Ranjandas <thejranjan@gmail.com>
2015-04-01 10:26:08 +1100

    New Commit
    Brought test_connectivity function inside the class. Removed unused argparse import and related code.

--- zero_rpc_agent.py
+++ zero_rpc_agent.py
@@ -1,15 +1,9 @@
+#!/usr/bin/env python
 import socket
-import argparse
 import zerorpc
 
-parser = argparse.ArgumentParser(description='Firewall Test agent arguments')
-parser.add_argument("host",
-                    help='hostname of the machine in which it is running')
-# parser.add_argument("server_url", help='URL of the server hosting the rules')
-args = parser.parse_args()
-
-
-def test_connectivity(dest_ip, port, proto):
+class KickBackServer(object):
+    def test_connectivity(self,dest_ip, port, proto):
     if(proto == "TCP"):
         proto_type = socket.SOCK_STREAM
     elif(proto == "UDP"):
@@ -21,19 +15,11 @@ def test_connectivity(dest_ip, port, proto):
     sock.close()
     print str(dest_ip) + " : " + str(port) + " : " + proto + " : " + str(status)
     return status
-
-
-class KickBackServer(object):
+    
     def get_firewall_status(self, destination_ip, port, protocol):
         print destination_ip, port
-        status = test_connectivity(destination_ip, port, protocol)
-        # dict connection_status = {
-        #     'source': args.host,
-        #     'destination': destination_ip,
-        #     'destination_port': port,
-        #     'protocol': protocol,
-        #     'status': status
-        # }
+        status = self.test_connectivity(destination_ip, port, protocol)
+       
         # return connection_status
         return status
 
